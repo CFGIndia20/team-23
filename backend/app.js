8b1881465d810f4c4b38a47528928b372d542762
krish1201 <krishnashah.shah98@gmail.com>
2020-07-25 19:24:33 +0530

    book slot(student)

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ db.once('open', ()=>{
 
 app.use("/", require('./routes/index'))
 app.use("/admin", require("./routes/admin"));
-app.use("/student", require("./routes/admin"));
+app.use("/student", require("./routes/student"));
 
 app.listen(process.env.PORT || 3000,"127.0.0.1",function(){
 	console.log("Server is Running.")
