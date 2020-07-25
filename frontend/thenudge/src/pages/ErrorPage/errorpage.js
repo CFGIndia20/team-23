322d5145c0d53806b0480ce14166e5f858fa631c
diksha-15 <dikshajain1504@gmail.com>
2020-07-25 22:52:42 +0530

    changes made in error page

--- frontend/thenudge/src/pages/ErrorPage/errorpage.js
+++ frontend/thenudge/src/pages/ErrorPage/errorpage.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 
 const ErrorPage = () => (
-    <div>Error Page</div>
+    <div>
+    <h3>404 page not found</h3>
+    <p>We are sorry but the page you are looking for does not exist.</p>
+    </div>
 )
-
 
 export default ErrorPage
