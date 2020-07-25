780b23f391a8396c0073a00e6c8ee4ee259f7eae
diksha-15 <dikshajain1504@gmail.com>
2020-07-25 23:55:27 +0530

    added viewjobs page

--- frontend/thenudge/src/pages/Recruiter/recruiter.js
+++ frontend/thenudge/src/pages/Recruiter/recruiter.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Tab, Tabs} from 'react-bootstrap';
 import AddJobs from '../../components/AddJobs/addjobs';
+import ViewJobs from '../../components/ViewJobs/ViewJobs';
 
 const Recruiter = () => (
     <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
         <Tab eventKey="addjobs" title="Available Jobs">
-          <addjobs/>
+          <AddJobs/>
+        </Tab>
+        <Tab eventKey="viewSlots" title="View Slots">
+            <ViewJobs/>
         </Tab>
     </Tabs>
 )
