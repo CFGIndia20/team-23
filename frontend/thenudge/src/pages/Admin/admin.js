780cd7ab6460258e0c1885522a92893aaf75b236
saurabhrane1199 <36326161+saurabhrane1199@users.noreply.github.com>
2020-07-26 00:23:25 +0530

    makeSlots viewSlots

--- frontend/thenudge/src/pages/Admin/admin.js
+++ frontend/thenudge/src/pages/Admin/admin.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import {Tab, Tabs, Container} from 'react-bootstrap';
+import { Tab, Tabs, Container } from 'react-bootstrap';
 import MakeSlots from '../../components/MakeSlots/makeSlots';
 import ViewSlots from '../../components/ViewSlots/ViewSlots';
 import AddTeacherRecruiter from '../../components/AddTeacherRec/addTeacherRecruiter';
 
-const Admin = () => (
-    <Container>
-    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-        <Tab eventKey="makeSlots" title="Make Slots">
-            <MakeSlots/>
-        </Tab>
-        <Tab eventKey="viewSlots" title="View Slots">
-            <ViewSlots/>
-        </Tab>
-        <Tab eventKey="addTeacher" title="Add Teacher/Recruiter">
-            <AddTeacherRecruiter/>
-        </Tab>
-    </Tabs>
-    </Container>
-)
+class Admin extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Tabs defaultActiveKey="makeSlots" id="uncontrolled-tab-example">
+                    <Tab eventKey="makeSlots" title="Make Slots">
+                        <MakeSlots />
+                    </Tab>
+                    <Tab eventKey="viewSlots" title="View Slots">
+                        <ViewSlots />
+                    </Tab>
+                    <Tab eventKey="addTeacher" title="Add Teacher/Recruiter">
+                        <AddTeacherRecruiter />
+                    </Tab>
+                </Tabs>
+            </Container>
+        )
+    }
+
+
+} 
 
 
 export default Admin
